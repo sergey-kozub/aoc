626271022cfe64d01ff1c907efae39fcc46d9361
Sergey Kozub <sergey@kozub.email>
2020-12-05 08:51:47 +0100

    day5

---
+++ day5.py
@@ -0,0 +1,6 @@
+items = list(open('day5.txt'))
+tt = str.maketrans({'B':'1','F':'0','R':'1','L':'0'})
+decode = lambda seat: int(seat.translate(tt), 2)
+seats = set(map(decode, items))
+all_ = set(range(min(seats), max(seats)))
+print(all_ - seats)
