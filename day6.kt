a2a7d246b94e64c61539e7444dd03a2d95015777
Sergey Kozub <sergey@kozub.email>
2020-12-06 07:51:05 +0100

    day6 kotlin

---
+++ day6.kt
@@ -0,0 +1,8 @@
+import java.io.File
+
+fun main() {
+    val input = File("day6.txt").readText().split("\n\n")
+    val groups = input.map { t -> t.split("\n").map { s -> s.toSet() } }
+    println(groups.map { g -> g.reduce { a, b -> a union b }.size }.sum())
+    println(groups.map { g -> g.reduce { a, b -> a intersect b }.size }.sum())
+}
